8b6e40d7c8e653a922260ccf9e0126b7af6b2ae6
SANAYEE <dudtkd0219@gmail.com>
2022-01-08 09:13:28 +0900

    오래 걸렸다...

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -1,15 +1,18 @@
 package com.example.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index(){
-        return "hello my name is ysPark"; 
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String first(Model model){
+        System.out.println("hello 제발..");
+        model.addAttribute("name", "내 이름은 박영상!");
+        return "first"; 
     }
     
 }
