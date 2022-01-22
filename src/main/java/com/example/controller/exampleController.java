6b349c8a4ecfb2145bd8b3adad657c6b65149cf6
SANAYEE <dudtkd0219@gmail.com>
2022-01-22 09:42:09 +0900

    롬복 사용 예시 컨트롤러

---
+++ src/main/java/com/example/controller/exampleController.java
@@ -0,0 +1,15 @@
+package com.example.controller;
+
+import com.example.VO.ExampleVO;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class exampleController {
+    
+    public static void ex(){
+        ExampleVO evo = new ExampleVO();
+        evo.setId("오~");
+        System.out.println(evo.getId());
+    }
+}
