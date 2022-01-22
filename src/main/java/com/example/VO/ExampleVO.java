dbe537ae5fc9e2127e51648ea072fbdc1950c51c
SANAYEE <dudtkd0219@gmail.com>
2022-01-22 09:41:25 +0900

    Data 어노테이션으로 쉽게 롬복 getter setter 설정하기

--- src/main/java/com/example/VO/ExampleVO.java
+++ src/main/java/com/example/VO/ExampleVO.java
@@ -1,10 +1,8 @@
 package com.example.VO;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter
-@Setter
+@Data
 public class ExampleVO {
     private String id;
     private String password;
