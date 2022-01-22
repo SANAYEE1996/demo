57cfdc016df273332309dd6e12f659e3e91523a6
SANAYEE <dudtkd0219@gmail.com>
2022-01-22 09:30:27 +0900

    롬복 예시 VO

---
+++ src/main/java/com/example/VO/ExampleVO.java
@@ -0,0 +1,12 @@
+package com.example.VO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ExampleVO {
+    private String id;
+    private String password;
+    private String name;
+}
